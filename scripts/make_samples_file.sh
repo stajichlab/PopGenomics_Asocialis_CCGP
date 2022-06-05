4d1ab3039e5383943320dc17b9c31b306223de97
Jason Stajich <jasonstajich.phd@gmail.com>
2022-06-05 12:25:06 -0700

    fixed the LXXX number in sample file

--- scripts/make_samples_file.sh
+++ scripts/make_samples_file.sh
@@ -2,7 +2,7 @@
 echo "STRAIN,FILEBASE" > samples.csv
 pushd input
 #ls *R1.fastq.gz | perl -p -e 's/((\S+)_R1\.fastq\.gz)/$2,$2_R[12].fastq.gz/' >> ../samples.csv
-ls *R1_001.fastq.gz | perl -p -e 's/(((\S+)_S\d+)_L\d+_R1_001\.fastq\.gz)/$3,$2\_R[12]_001.fastq.gz/'  >> ../samples.csv
+ls *R1_001.fastq.gz | perl -p -e 's/(((\S+)_S\d+)_(L\d+)_R1_001\.fastq\.gz)/$3,$2\_$4\_R[12]_001.fastq.gz/'  >> ../samples.csv
 popd
 perl scripts/fix_CCGP_multirun.pl samples.csv > sample_fix.csv
 mv sample_fix.csv samples.csv
